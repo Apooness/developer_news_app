7b54d02246230c3d1bc2edb7d77d0a272cdab666
Apooness <mustafaakil49@gmail.com>
2020-12-30 14:01:15 +0300

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/views/main_page.dart';
-import 'package:news_app/views/sign_in_page.dart';
+
+import 'views/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,14 +14,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Akil Haber',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           centerTitle: true,
           color: Colors.green,
         ),
       ),
-      home: MainPage(),
+      home: HomePage(),
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () async {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainPage(),
+        ),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Container(
+            child: Image.asset(
+              "assets/multicamp.png",
+            ),
+          ),
+        ));
   }
 }
