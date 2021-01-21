111948dc42c93148ddd07a1c1ea3a14c5722acf9
Apooness <mustafaakil49@gmail.com>
2021-01-21 11:02:40 +0300

    newsPage

--- lib/views/news_page.dart
+++ lib/views/news_page.dart
@@ -1,69 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/states/news_state.dart';
-import 'package:provider/provider.dart';
+import 'package:news_app/data/http.dart';
+import 'package:news_app/views/setting_page.dart';
+import 'package:webfeed/domain/rss_feed.dart';
 
-class NewsPage extends StatelessWidget {
+import 'detail_page.dart';
+
+class NewsPage extends StatefulWidget {
+  NewsPage({Key key}) : super(key: key);
+
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  HttpService _service;
+  TextEditingController _searchController;
+
+  @override
+  void initState() {
+    _service = HttpService();
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(),
-      body: _buildConsumer(),
-    );
-  }
-
-  AppBar _buildAppBar() => AppBar(
-        title: Text(
-          'RSS Feed Provider',
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(hintText: "Ara"),
         ),
-      );
-
-  Consumer<NewsState> _buildConsumer() {
-    return Consumer<NewsState>(
-      builder: (context, model, child) => model.isLoading ? _buildCenter() : _buildContainer(model),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  getNews();
+                });
+              }),
+          IconButton(icon: Icon(Icons.settings), onPressed: () => goToSettingPage()),
+        ],
+      ),
+      body: getNews(),
     );
   }
 
-  Center _buildCenter() {
-    return Center(
-      child: CircularProgressIndicator(),
-    );
-  }
-
-  Container _buildContainer(NewsState model) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: _buildListView(model),
-    );
-  }
-
-  ListView _buildListView(NewsState model) {
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: model.rssFeeds.items.length,
-      itemBuilder: (context, int index) {
-        return _buildCard(model, index);
+  getNews() {
+    return FutureBuilder(
+      future: _service.getNews(),
+      builder: (context, AsyncSnapshot<RssFeed> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Text("Hata oluştu. Hata:${snapshot.error}");
+        }
+        if (!snapshot.hasData && snapshot.data.items.length == 0) {
+          return Text("No Data");
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final news = snapshot.data.items[index];
+            return InkWell(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => DetailPage(url: news.link),
+                ),
+              ),
+              child: Card(
+                child: Row(
+                  children: [
+                    Image(
+                      fit: BoxFit.cover,
+                      height: 100,
+                      width: 150,
+                      image: NetworkImage(news.enclosure.url),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        news.title,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
       },
     );
   }
 
-  Card _buildCard(NewsState model, int index) {
-    return Card(
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: _buildListTile(model, index),
-    );
-  }
-
-  ListTile _buildListTile(NewsState model, int index) {
-    return ListTile(
-      title: Text(
-        'Title: ' + model.rssFeeds.items[index].title,
-      ),
-      subtitle: Text(
-        'Link: ' + model.rssFeeds.items[index].link,
+  goToSettingPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => SettingPage(),
       ),
     );
   }
